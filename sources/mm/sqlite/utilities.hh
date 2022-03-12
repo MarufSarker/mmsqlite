d0134ea0ebeb89cf3e8c07492f76f3d529096ed6
Maruf Sarker <maruf.sarker@gmail.com>
2022-03-13 05:51:36 +0600

    add utilities.hh

---
+++ sources/mm/sqlite/utilities.hh
@@ -0,0 +1,40 @@
+/*
+ * mmsqlite
+ * Copyright (C) 2022  Maruf Sarker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#pragma once
+
+#include <string>
+
+namespace mm
+{
+namespace sqlite
+{
+void valid_sqlite_identifier(std::string const& identifier);
+
+
+std::string to_string(int const& value);
+
+int to_int(std::string const& value);
+
+
+std::string to_string(double const& value);
+
+double to_double(std::string const& value);
+} // namespace sqlite
+} // namespace mm
